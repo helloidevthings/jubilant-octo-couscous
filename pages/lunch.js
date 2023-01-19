456430f51cc4e6cf0068de89106bcdb0302ac951
Joyanna Hirst <helloidevthings@gmail.com>
2023-01-19 11:13:37 -0500

    lunch thing

---
+++ pages/lunch.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const LunchOptions = [
+  'Potschkee',
+  "Clancy's",
+  'Tomatohead',
+  'Marble City Market',
+];
+
+const MainWrapper = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+`;
+
+const Lunch = () => {
+  const [option, setLunch] = useState(null);
+
+  const handleLunch = () => {
+    const random = Math.floor(Math.random() * LunchOptions.length);
+    setLunch(LunchOptions[random]);
+  };
+
+  return (
+    <MainWrapper>
+      <h1>Lunch</h1>
+      <ul>
+        {LunchOptions.map((option) => (
+          <li key={option}>{option}</li>
+        ))}
+      </ul>
+      <button onClick={handleLunch}>Select Option</button>
+      <h3>{option === null ? null : option}</h3>
+    </MainWrapper>
+  );
+};
+
+export default Lunch;
