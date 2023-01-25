1295908cc2e9538392cbf27e0ea598859802c88d
Joyanna Hirst <helloidevthings@gmail.com>
2023-01-25 16:15:30 -0500

    silly things

--- pages/lunch.js
+++ pages/lunch.js
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
+// const LunchOptions = [
+//   { name: 'Potschkee', id: 1 },
+//   { name: "Clancy's", id: 2 },
+//   { name: 'Marble City Market', id: 3 },
+//   { name: 'Kefi', id: 4 },
+//   { name: 'FinTwo', id: 5 },
+//   { name: "Sweet P's", id: 6 },
+//   { name: 'Kaizen', id: 7 },
+//   { name: 'Jig & Reel', id: 8 },
+//   { name: "Yassin's", id: 9 },
+//   { name: 'Soccer Taco', id: 10 },
+//   { name: 'Tomatohead', id: 11 },
+//   { name: 'Babalu', id: 12 },
+// ];
+
 const LunchOptions = [
-  // 'Potschkee',
-  // "Clancy's",
-  // 'Tomatohead',
-  // 'Marble City Market',
-  'Kefi',
+  'Potschkee',
   "Clancy's",
-  'FinTwoo',
+  'Marble City Market',
+  'Kefi',
+  'FinTwo',
   "Sweet P's",
   'Kaizen',
   'Jig & Reel',
@@ -28,24 +41,72 @@ const MainWrapper = styled.main`
   text-align: left;
 `;
 
+const LunchButton = styled.button`
+  background: blue;
+
+  [aria-selected='true'] {
+    background: orange;
+  }
+`;
+
 const Lunch = () => {
   const [option, setLunch] = useState(null);
+  const [selected, setSelected] = useState([]);
+  const [active, setActive] = useState(false);
 
   const handleLunch = () => {
-    const random = Math.floor(Math.random() * LunchOptions.length);
-    setLunch(LunchOptions[random]);
+    const random = Math.floor(Math.random() * selected.length);
+    setLunch(selected[random]);
+  };
+
+  // const handleSelect = () => {
+  //   setSelected([...selected, option]);
+  //   console.log(selected);
+  //   handleLunch;
+  // };
+
+  const toggleSelected = (e) => {
+    const option = e.target.innerText;
+    if (selected.includes(option)) {
+      setSelected(selected.filter((item) => item !== option));
+    } else {
+      setSelected([...selected, option]);
+      setActive(true);
+    }
+  };
+
+  const clearSelect = () => {
+    setLunch(null);
+    setSelected([]);
+    console.log(selected);
   };
 
   return (
     <MainWrapper>
       <h1>Lunch</h1>
+      <button onClick={clearSelect}>Clear Select</button>
       <ul>
-        {LunchOptions.map((option) => (
-          <li key={option}>{option}</li>
-        ))}
+        {LunchOptions.map((option, i) => {
+          return (
+            <li key={i}>
+              <LunchButton
+                aria-selected={active === true ? true : false}
+                onClick={toggleSelected}
+              >
+                {option}
+              </LunchButton>
+            </li>
+          );
+        })}
       </ul>
+      <h4>New List</h4>
+      <p>
+        {selected.map((option, i) => {
+          return <span key={i}>{option === null ? null : option} • </span>;
+        })}
+      </p>
       <button onClick={handleLunch}>Select Option</button>
-      <h3>{option === null ? null : option}</h3>
+      <h2>{option === null ? 'Select an option' : option}</h2>
     </MainWrapper>
   );
 };
