b34d516ca576199a5c1877946866a68dda0c887c
Joyanna Hirst <helloidevthings@gmail.com>
2023-07-24 15:28:40 -0400

    animations complete

--- pages/RegalConfetti.js
+++ pages/RegalConfetti.js
@@ -48,6 +48,10 @@ const IconWrap = styled.div`
     height: 100%;
   }
 `;
+const ConfettiWrap = styled.div`
+  padding: 1.5rem;
+  position: relative;
+`;
 
 const IconWrapGem = styled(IconWrap)`
   border: 3px solid #5b9340;
@@ -90,22 +94,23 @@ const RegalConfetti = () => {
 
   return (
     <MainWrap>
-      <IconWrap>
+      <ConfettiWrap>
         <Confetti startAnimation={playConfetti} />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 27 21"
-          width="50px"
-          height="50px"
-        >
-          <path
-            d="M23.82 10.5c0-1.33 1.161-2.419 2.58-2.419V3.244c0-1.33-1.161-2.419-2.58-2.419H3.18C1.761.825.613 1.913.613 3.244V8.08c1.419 0 2.567 1.089 2.567 2.419S2.032 12.919.6 12.919v4.837c0 1.33 1.161 2.419 2.58 2.419h20.64c1.419 0 2.58-1.088 2.58-2.419V12.92c-1.419 0-2.58-1.089-2.58-2.419Zm-5.702 5.805L13.5 13.523l-4.618 2.782 1.393-4.983L6.031 8.07l5.47-.302L13.5 3.002l1.987 4.777 5.47.302-4.245 3.254 1.406 4.97Z"
-            fill="#fff"
-          />
-        </svg>
-      </IconWrap>
-      <br />
+        <IconWrap>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 27 21"
+            width="50px"
+            height="50px"
+          >
+            <path
+              d="M23.82 10.5c0-1.33 1.161-2.419 2.58-2.419V3.244c0-1.33-1.161-2.419-2.58-2.419H3.18C1.761.825.613 1.913.613 3.244V8.08c1.419 0 2.567 1.089 2.567 2.419S2.032 12.919.6 12.919v4.837c0 1.33 1.161 2.419 2.58 2.419h20.64c1.419 0 2.58-1.088 2.58-2.419V12.92c-1.419 0-2.58-1.089-2.58-2.419Zm-5.702 5.805L13.5 13.523l-4.618 2.782 1.393-4.983L6.031 8.07l5.47-.302L13.5 3.002l1.987 4.777 5.47.302-4.245 3.254 1.406 4.97Z"
+              fill="#fff"
+            />
+          </svg>
+        </IconWrap>
+      </ConfettiWrap>
       <h1>Regal Confetti</h1>
       <br />
       <button onClick={() => handleConfetti(true)}>Test Animation</button>
