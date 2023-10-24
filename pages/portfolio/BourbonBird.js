05eb240ddec5fc637fa6cde8d59435321f94f0be
Joyanna Hirst <helloidevthings@gmail.com>
2023-10-24 15:50:08 -0400

    quick fix remove project data

--- pages/portfolio/BourbonBird.js
+++ pages/portfolio/BourbonBird.js
@@ -12,7 +12,6 @@ const BourbonBird = () => {
   return (
     <Wrapper>
       <h1>BourbonBird</h1>
-      <img src="https://res.cloudinary.com/labofthingsimages/image/upload/v1670295204/Portfolio_Images/7d735d55219983.597b2c24b5436_1_wdct5w.png" />
     </Wrapper>
   );
 };
--- pages/portfolio/DataVisualization.js
+++ pages/portfolio/DataVisualization.js
@@ -8,10 +8,6 @@ const DataVisualization = () => {
   return (
     <Wrapper>
       <h1>DataVisualization</h1>
-      <img
-        src="https://res.cloudinary.com/labofthingsimages/image/upload/v1678908929/Portfolio_Images/Screen_Shot_2023-03-15_at_3.34.41_PM_kp1c2t.png"
-        alt="Image of a guy using a VR headset in a living room with a cat"
-      />
     </Wrapper>
   );
 };
