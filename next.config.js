de36dc9782f34b1123cfe3dd3871b22b5262b097
Joyanna Hirst <helloidevthings@gmail.com>
2023-10-25 10:17:56 -0400

    fix cloudinary api etc

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { domains: ['res.cloudinary.com'] },
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 module.exports = nextConfig;
