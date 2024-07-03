6c063873db7dd368909507ce38328025372d320e
Joyanna Hirst <helloidevthings@gmail.com>
2024-07-03 15:27:16 -0400

    dead wolf final

---
+++ pages/projects/DeadWolf.js
@@ -0,0 +1,191 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
+// import DeadWolfIcon from '../../components/Icons/DeadWolfIcon';
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  max-width: 100vw;
+  max-height: 100vh;
+  margin: auto;
+  color: #fff;
+  font-family: 'AzoSansWebMed', Helvetica, sans-serif;
+  text-align: center;
+  display: grid;
+  place-items: center;
+  background: linear-gradient(
+    to right bottom,
+    ${(props) => props.$colors.light},
+    ${(props) => props.$colors.dark},
+    #000
+  );
+  z-index: 0;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  justify-items: center;
+
+  h1,
+  h2,
+  h3 {
+    margin: 1em auto;
+  }
+`;
+const TextureImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  object-fit: cover;
+  mix-blend-mode: multiply;
+  z-index: -1;
+`;
+
+const PopUpWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  padding: 1em 3em;
+  text-align: center;
+  background: #0000008f;
+  border-radius: 1rem;
+  box-shadow: 6px 5px 17px 2px #0000006e;
+  backdrop-filter: blur(7px);
+`;
+
+const ToggleWrap = styled.div`
+  position: relative;
+  height: 100px;
+  width: 300px;
+  border-radius: 50px;
+  box-shadow: 0px 5px 13px 5px #000;
+  /* background: linear-gradient(to right, #232526, #181f27cc); */
+  z-index: 10;
+
+  /* &:after {
+    content: '';
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
+    background: linear-gradient(to right, #f9d423, #ed213a);
+    top: 0;
+    left: ${(props) => props.$colors.X};
+    transform: ${(props) => props.$colors.r};
+    transition: left 0.4s cubic-bezier(1, -0.35, 0.4, 1.19);
+    z-index: -1;
+  } */
+
+  &:after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 0;
+    border-radius: 50px;
+    z-index: -1;
+    border: 2px solid #000;
+    background: linear-gradient(to right, #23252691, #414345d1);
+  }
+
+  /* svg {
+    position: absolute;
+    height: 100px;
+    width: 100px;
+    top: 0;
+    left: ${(props) => props.$colors.X};
+  } */
+`;
+const ToggleImage = styled(Image)`
+  position: absolute;
+  width: 150px;
+  height: 150px;
+  top: -25px;
+  left: ${(props) => props.$colors.left};
+  transform: ${(props) => props.$colors.deg};
+  z-index: 0;
+  transition: all 0.4s ease-in-out;
+`;
+
+const ToggleButton = styled.button`
+  position: relative;
+  width: 50%;
+  height: 100%;
+  background: transparent;
+  border: none;
+  z-index: 1000;
+`;
+
+const DeadWolf = () => {
+  const colors = [
+    {
+      name: 'good choice peanut',
+      light: '#ED213A',
+      dark: '#93291E',
+      left: '-25px',
+      deg: 'rotate(-1deg)',
+    },
+    {
+      name: 'him? really?',
+      light: '#F9D423',
+      dark: '#EFA812',
+      left: '58%',
+      deg: 'rotate(359deg)',
+    },
+  ];
+
+  const [currentColor, setMyColor] = useState({
+    name: 'Hugh will you choose?',
+    light: '#203A43',
+    dark: '#0F2027',
+    left: '-25px',
+    top: '-25px',
+    deg: 'rotate(0deg)',
+  });
+
+  const handleColors = ({ name, light, dark, left, deg }) => {
+    setMyColor({ name, light, dark, left, deg });
+  };
+
+  return (
+    <Wrapper $colors={currentColor}>
+      <TextureImage
+        src="https://res.cloudinary.com/labofthingsimages/image/upload/v1720032456/DP-Texture_mqyrig.jpg"
+        alt="Seat map and selection screens"
+        width={500}
+        height={500}
+      />
+      <PopUpWrapper>
+        <h1>cHUGHse your favorite...</h1>
+        <ToggleWrap $colors={currentColor}>
+          <ToggleImage
+            $colors={currentColor}
+            src="https://res.cloudinary.com/labofthingsimages/image/upload/v1720032337/toggle2_qllosy.png"
+            alt="Seat map and selection screens"
+            width={150}
+            height={150}
+          />
+          {colors.map(({ name, light, dark, left, deg }, i) => (
+            <ToggleButton
+              key={light}
+              onClick={() =>
+                handleColors({
+                  name: name,
+                  light: light,
+                  dark: dark,
+                  left: left,
+                  deg: deg,
+                })
+              }
+            ></ToggleButton>
+          ))}
+          {/* <DeadWolfIcon /> */}
+        </ToggleWrap>
+        <h3>{currentColor.name}</h3>
+      </PopUpWrapper>
+    </Wrapper>
+  );
+};
+
+export default DeadWolf;
