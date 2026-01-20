d3231484bb019fdedd620838895e6bba2e504c50
helloidevthings <helloidevthings@gmail.com>
2026-01-20 06:41:22 -0500

    updates

--- components/ThemeButton.js
+++ components/ThemeButton.js
@@ -1,5 +1,5 @@
 import { useTheme } from 'next-themes';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 // import Moon from './Icons/Moon';
 // import Sun from './Icons/Sun';
@@ -19,6 +19,17 @@ const ColorToggle = styled.button`
 
 const ThemeButton = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Only render after mount to avoid hydration mismatch
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <ColorToggle>Theme</ColorToggle>;
+  }
+
   return (
     <ColorToggle onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
       {/* <Moon /> */}
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
-'use client';
-// import Providers from '../public/Providers';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
